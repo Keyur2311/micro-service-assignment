5874a735d5ba222700f0e96de3a08841f980650e
Keyur Patel <patelkeyur2311@gmail.com>
2025-01-09 13:21:30 +0530

    fixed a mistake

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.post('/users', addUser)
 router.get('/users', fetchUsers)
-router.delete('users/:userId', deleteUser)
+router.delete('/users/:userId', deleteUser)
 
 module.exports = router
 
